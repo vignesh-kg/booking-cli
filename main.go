15959d5a210d721359ccc6c9a1243a69d8a1ac47
vignesh-kg <vigneshkg515@gmail.com>
2024-12-26 12:59:34 +0200

    Structs example

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"booking-cli/constants"
+	"booking-cli/structs"
 )
 
 const totalTickets = 50
@@ -11,4 +12,11 @@ var availableTickets = 50
 func main() {
 	fmt.Printf("Welcome to %v\n", constants.ConferenceName)
 	fmt.Printf("We have total %v tickets. Available %v\n", totalTickets, availableTickets)
+	fmt.Println("Please enter your userName:")
+	var user structs.User
+	fmt.Scan(&user.Username)
+	fmt.Println("Please enter number of Tickets you wish to purchase:")
+	fmt.Scan(&user.PurchasedTickets)
+	fmt.Printf("User %v purchased %v tickets\n", user.Username, user.PurchasedTickets)
+	fmt.Printf("Remaining Tickets : %d", availableTickets-int(user.PurchasedTickets))
 }
---
+++ structs/user.go
@@ -0,0 +1,6 @@
+package structs
+
+type User struct{
+	Username string
+	PurchasedTickets uint
+}
